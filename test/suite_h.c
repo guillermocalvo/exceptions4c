d05a6087b5261cc45e567132553a8a7eddf236c7
Guillermo Calvo <guillermocalvo@yahoo.com>
2010-11-22 19:39:32 +0000

    Fixed documentation for Integration test suite.

--- test/suite_h.c
+++ test/suite_h.c
@@ -3,12 +3,11 @@
 
 # define NEW TEST_SUITE(h, "Integration", "\
 \
-This suite of tests checks the behavior of the library when the program \
-receives a signal from the operating system. If no signal mapping is provided, \
-then the behavior is implementation-defined. However, if the signal mappings \
-have been set up, then the library must map the received signal to the \
-appropriate exception. Some of the tests will <code>catch</code> it, some \
-others won't.\
+This suite of tests checks the behavior of the library when \
+<code>e4c_reusing_context</code> is used. This integration mechanism lets \
+library developers make use of exceptions4c, regardless of whether the clients \
+are unaware of the exception handling system. Some of the tests will propagate \
+exceptions to the caller, some others will return a status code. \
 \
 ")
 
