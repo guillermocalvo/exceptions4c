4fc88feef44a0f02a66c10c6b546cb10a22ffd44
Guillermo Calvo <guillermocalvo@users.noreply.github.com>
2011-01-26 20:26:00 +0000

    Removed "POSIX warning" from requirements z01, z02 and z03.

--- test/test_z01.c
+++ test/test_z01.c
@@ -1,22 +1,13 @@
 
 # include "testing.h"
 
-# ifdef _POSIX_SOURCE
-#	define DEFINE_POSIX_SOURCE
-# else
-#	define DEFINE_POSIX_SOURCE \
-		"<div class=\"paragraph\"><strong>" \
-			"If your platform provides the macro <code>WEXITSTATUS</code>," \
-			" please define <code>_POSIX_SOURCE</code> and compile again." \
-		"</strong></div>"
-# endif
 
 DEFINE_REQUIREMENT(
 	z01,
 	"Platform behavior when returning EXIT_SUCCESS",
 	"This test simply yields <code>EXIT_SUCCESS</code> as exit code.",
 	SEVERITY_CRITICAL,
-	"The test framework is unable to detect when a test returns <code>EXIT_SUCCESS</code> exit code." DEFINE_POSIX_SOURCE,
+	"The test framework is unable to detect when a test returns <code>EXIT_SUCCESS</code> exit code.",
 	EXIT_SUCCESS,
 	NULL,
 	NULL
