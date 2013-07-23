d195065e080cdc49c1e56b44f28fefb1fa076878
Guillermo Calvo <guillermocalvo@users.noreply.github.com>
2013-07-23 18:20:08 +0000

    Updated optional source files (version number and license).

--- etc/e4c_rsc.c
+++ etc/e4c_rsc.c
@@ -4,21 +4,22 @@
  *
  * exceptions4c automatic resource handling source code file
  *
- * @version		1.2
+ * @version		2.0
  * @author		Copyright (c) 2012 Guillermo Calvo
  *
- * This is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the **GNU Lesser General Public License** as published by the
+ * *Free Software Foundation*, either version 3 of the License, or (at your
+ * option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This software is distributed in the hope that it will be useful, but
+ * **WITHOUT ANY WARRANTY**; without even the implied warranty of
+ * **MERCHANTABILITY** or **FITNESS FOR A PARTICULAR PURPOSE**. See the
+ * [GNU Lesser General Public License](http://www.gnu.org/licenses/lgpl.html)
+ * for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
