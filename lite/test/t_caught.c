0ed739401d915d8c91b97ff07bb13982e0af4246
Guillermo Calvo <guillermocalvo@yahoo.com>
2016-03-05 02:50:50 +0100

    Add a bunch of test cases for the lightweight version

---
+++ lite/test/t_caught.c
@@ -0,0 +1,18 @@
+
+#include "testing.h"
+
+
+E4C_DEFINE_EXCEPTION(CustomException, "Custom exception.", RuntimeException);
+
+
+TEST_CASE(test_caught_exception){
+
+	E4C_TRY{
+
+		E4C_THROW(CustomException, "This is a custom exception");
+
+	}E4C_CATCH(CustomException){
+
+		printf("The custom exception was caught: %s\n", E4C_EXCEPTION.type->name);
+	}
+}
---
+++ lite/test/t_uncaught.c
@@ -0,0 +1,8 @@
+
+#include "testing.h"
+
+
+TEST_CASE(test_uncaught_exception){
+
+	E4C_THROW(NullPointerException, "This is an uncaught exception");
+}
