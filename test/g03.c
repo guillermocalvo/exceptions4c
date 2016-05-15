4cdf8cce82f63c12e31adb7b36126d3a8d8614fd
Guillermo Calvo <guillermocalvo@yahoo.com>
2016-05-15 20:50:48 +0200

    Fix typo

--- test/g03.c
+++ test/g03.c
@@ -4,7 +4,7 @@
 
 DEFINE_TEST(
 	g03,
-	"Signal SIGABR",
+	"Signal SIGABRT",
 	"This test calls <code>abort</code>; the library signal handling is disabled, so the behavior of the program will be implementation-defined (typically the program will terminate abruptly).",
 	"This functionality relies on the <a href=\"#requirement_z06\"><strong>platform's behavior when aborting program</strong></a>.",
 	EXIT_WHATEVER,
